e9244b34b193ca844461d477838e6dca06a64861
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-20 10:35:14 -0700

    Downloaded and integrated Rune SDK

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import rune from "vite-plugin-rune"
+import path from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "", // Makes paths relative
+  plugins: [react(), rune({ logicPath: path.resolve("./src/logic.ts") })],
 })
