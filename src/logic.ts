fe33b0dc0ee253b6108added2880a9ffb5c71e32
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-21 15:52:26 -0700

    Added some comments and made array of the turn order

--- src/logic.ts
+++ src/logic.ts
@@ -8,8 +8,7 @@ export interface compPlayer {
 export interface GameState {
   allPlayerIds: string[]
   allComps: Array<compPlayer>
-  playersSpunThisRound: string[]
-  currentTurnPlayer: string
+  turnOrder: string[]
   count: number
   gameOver: boolean
 }
@@ -32,19 +31,13 @@ Rune.initLogic({
   maxPlayers: 4,
   setup: (allPlayerIds: string[]): GameState => ({
     allPlayerIds,
-    allComps: (Array(12 - allPlayerIds.length)
+    // array of all computer players AKA bots
+    allComps: (Array(12 - allPlayerIds.length) 
       .fill('comp')
       .map((el, i) => `${el}${i}`)
       // TODO: perhaps add other properties to comp objects
-      .reduce((acc, curr) => ({...acc, [curr]: {isUsingBomb: false, isDead: false}}), {}) as Array<compPlayer>)
-
-
-
-
-
-      ,
-    playersSpunThisRound: [],
-    currentTurnPlayer: [
+      .reduce((acc, curr) => ({...acc, [curr]: {isUsingBomb: false, isDead: false}}), {}) as Array<compPlayer>),
+    turnOrder: [
       ...allPlayerIds,
       ...Array(12 - allPlayerIds.length)
         .fill('comp')
@@ -52,19 +45,33 @@ Rune.initLogic({
     ]
       .map((value: string) => ({ value, sort: Math.random() }))
       .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value)[0],
+      .map(({ value }) => value),
     count: 0,
     gameOver: false,
   }),
   actions: {
-    increment: ({ amount }, { game }) => {
+    increment: ({ amount}, { game }) => {
       game.count += amount
     },
     spinBottle: ({ game, playerId } ) => {
-      if (playerId !== game.currentTurnPlayer)
+      if (playerId !== game.turnOrder[0]) {
         throw Rune.invalidAction()
+      }
+      
+
+      // players all have objects that say if they are dead or using bomb
+      // take turn
+      //everyone decide if they are using a bomb (comps random 50/50)
+      //once everyone decides (there should eventually be a timer), the bottle spins
+      //randomly select player from turnOrder list
+      // if playerId matches playerId selected for kiss, check if isUsingBomb is true. 
+      //if so, kisser isDead: true. If isUsingBomb: false, kissed isDead: true. 
+      //If unkissed isUsingBomb: true, then their isDead = true
+      // All player with isDead: true are eliminated.
+      // 1. if there are no more people players left game over and no one wins.
+      // 2. If there is one person and no more computers left, that person wins!
+      // If neither 1 or 2 are true, splice dead players from turnOrder array and continue.
     
-        game.playersSpunThisRound.push(playerId);
     }
   },
   events: {
