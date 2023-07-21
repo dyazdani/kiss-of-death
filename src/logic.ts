2ad805acdec61f4f32636b68293959b5c893acd7
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-21 10:46:33 -0700

    Added some logic for player for player turns

--- src/logic.ts
+++ src/logic.ts
@@ -1,11 +1,16 @@
 import type { RuneClient } from "rune-games-sdk/multiplayer"
 
 export interface GameState {
+  allPlayerIds: string[]
+  playersSpunThisRound: string[]
+  currentTurnPlayer: string
   count: number
+  gameOver: boolean
 }
 
 type GameActions = {
   increment: (params: { amount: number }) => void
+  spinBottle: (params: {game: GameState, playerId: string}) => void
 }
 
 declare global {
@@ -19,13 +24,30 @@ export function getCount(game: GameState) {
 Rune.initLogic({
   minPlayers: 1,
   maxPlayers: 4,
-  setup: (): GameState => {
-    return { count: 0 }
-  },
+  setup: (allPlayerIds: string[]): GameState => ({
+    allPlayerIds,
+    playersSpunThisRound: [],
+    currentTurnPlayer: [
+      ...allPlayerIds,
+      ...Array(12 - allPlayerIds.length)
+        .fill('comp')
+        .map((el, i) => `${el}${i}`)
+    ]
+      .map((value: string) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value)[0],
+    count: 0,
+    gameOver: false,
+  }),
   actions: {
     increment: ({ amount }, { game }) => {
       game.count += amount
     },
+    spinBottle: ({ game, playerId } ) => {
+      if (playerId !== game.currentTurnPlayer)
+        throw Rune.invalidAction()
+    
+    }
   },
   events: {
     playerJoined: () => {
