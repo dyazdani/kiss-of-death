92f3dd112f02bfd70ad259e996d42b0cb5875ac3
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-27 07:18:56 -0700

    Reduced players to only 4 displayed

--- src/components/PlayerCircle.tsx
+++ src/components/PlayerCircle.tsx
@@ -12,18 +12,9 @@ const PlayerCircle = ({allPlayerIds, players}: PlayerCircleProps) => {
     return (
         <div className="circle-wrapper"> 
             <Player playerId={allPlayerIds[0]} circleDeg="deg-0" playerName={`${players[allPlayerIds[0]].displayName}`} />
-            <Player playerId="comp1" circleDeg="deg-30" playerName="C1" />
-            <Player playerId="comp2" circleDeg="deg-60" playerName="C2" />
             <Player playerId={allPlayerIds[1]} circleDeg="deg-90" playerName={`${players[allPlayerIds[1]].displayName}`} />
-            <Player playerId="comp3" circleDeg="deg-120" playerName="C3" />
-            <Player playerId="comp4" circleDeg="deg-150" playerName="C4" />
             <Player playerId={allPlayerIds[2]} circleDeg="deg-180" playerName={`${players[allPlayerIds[2]].displayName}`} />
-            <Player playerId="comp5" circleDeg="deg-210" playerName="C5" />
-            <Player playerId="comp6" circleDeg="deg-240" playerName="C6" />
             <Player playerId={allPlayerIds[3]} circleDeg="deg-270" playerName={`${players[allPlayerIds[3]].displayName}`} />
-            <Player playerId="comp7" circleDeg="deg-300" playerName="C7" />
-            <Player playerId="comp8" circleDeg="deg-330" playerName="C8" />
-
         </div>
 
     )
