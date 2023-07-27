aa746085380aaf7e1dbcb47c546682094f0b03f3
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-27 12:24:08 -0700

    Added avatars

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,10 +2,15 @@ interface PlayerProps {
     playerId: string
     circleDeg: string
     playerName: string
+    avatarUrl: string
 } 
 
-const Player = ({playerId, circleDeg, playerName}: PlayerProps) => {
-    return <div id={playerId} className={`circle ${circleDeg}`}>{playerName}</div>
+const Player = ({playerId, circleDeg, playerName, avatarUrl}: PlayerProps) => {
+    return (
+            <div id={playerId} className={`circle ${circleDeg}`}>{playerName}
+                <img className={`avatar ${playerId}`} src={avatarUrl}/>      
+            </div>
+    )
 }
 
 export default Player;
--- src/components/PlayerCircle.tsx
+++ src/components/PlayerCircle.tsx
@@ -11,10 +11,30 @@ interface PlayerCircleProps {
 const PlayerCircle = ({allPlayerIds, players}: PlayerCircleProps) => {
     return (
         <div className="circle-wrapper"> 
-            <Player playerId={allPlayerIds[0]} circleDeg="deg-0" playerName={`${players[allPlayerIds[0]].displayName}`} />
-            <Player playerId={allPlayerIds[1]} circleDeg="deg-90" playerName={`${players[allPlayerIds[1]].displayName}`} />
-            <Player playerId={allPlayerIds[2]} circleDeg="deg-180" playerName={`${players[allPlayerIds[2]].displayName}`} />
-            <Player playerId={allPlayerIds[3]} circleDeg="deg-270" playerName={`${players[allPlayerIds[3]].displayName}`} />
+            <Player 
+                playerId={allPlayerIds[0]} 
+                circleDeg="deg-0" 
+                playerName={`${players[allPlayerIds[0]].displayName}`}
+                avatarUrl={`${players[allPlayerIds[0]].avatarUrl}`} 
+            />
+            <Player 
+                playerId={allPlayerIds[1]} 
+                circleDeg="deg-90" 
+                playerName={`${players[allPlayerIds[1]].displayName}`}
+                avatarUrl={`${players[allPlayerIds[1]].avatarUrl}`}
+            />
+            <Player 
+                playerId={allPlayerIds[2]} 
+                circleDeg="deg-180" 
+                playerName={`${players[allPlayerIds[2]].displayName}`} 
+                avatarUrl={`${players[allPlayerIds[2]].avatarUrl}`}
+                />
+            <Player 
+                playerId={allPlayerIds[3]} 
+                circleDeg="deg-270" 
+                playerName={`${players[allPlayerIds[3]].displayName}`}
+                avatarUrl={`${players[allPlayerIds[3]].avatarUrl}`}
+                />
         </div>
 
     )
